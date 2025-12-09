c828a92a88aa5395e320cb31a550f9d8a8f5a69b
Ruslan Hedz <ruslangedz76@gmail.com>
2025-12-09 14:19:26 +0100

    final editing 3.0

--- init/init.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE django_migrations;
-CREATE DATABASE reg_log;
-
-GRANT ALL PRIVILEGES ON django_migrations.* TO 'admin'@'%';
-GRANT ALL PRIVILEGES ON reg_log.* TO 'admin'@'%';
-FLUSH PRIVILEGES;
